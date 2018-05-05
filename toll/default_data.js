2df8e33bdb5991a3e8171c747187c42b2e78a199
Deepak Sharma <sd.05jan@gmail.com>
2018-05-05 10:17:08 +0530

    Add files via upload

---
+++ toll/default_data.js
@@ -0,0 +1,17 @@
+var defaultDataArray = [
+{
+"Plaza_Name": "RIDCOR TOLL PLAZA",
+"Plaza_Address": "Malasar",
+"Plaza": "4",
+"Lane": "1",
+"Operator_Name": "Prem Chand",
+"Vehicle_Class": "MultiAxle",
+"Payment_Method": "Cash",
+"Pass_Type": "Single",
+"Journey": "Sardarshahar to Kishangarh",
+"Direction": "Southbound",
+"Amount_Paid": "Rs. 1,500.00",
+"Vehicle_Regn": "RJ09RT0799",
+"Emergency_Contact": "9799492501/1, 9799492501/2"
+}
+];
