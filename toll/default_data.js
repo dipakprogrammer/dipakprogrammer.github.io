71a40c05042838b194fbd356cb208e131ebc249a
Deepak Sharma <sd.05jan@gmail.com>
2018-05-05 20:06:44 +0530

    Add files via upload

--- toll/default_data.js
+++ toll/default_data.js
@@ -1,4 +1,4 @@
-var defaultDataArray = [
+var defaultDataArray_1 = [
 {
 "Plaza_Name": "RIDCOR TOLL PLAZA",
 "Plaza_Address": "Malasar",
@@ -14,5 +14,20 @@ var defaultDataArray = [
 "Vehicle_Regn": "RJ09 RT0799",
 "Emergency_Contact_1": "9799042301/2",
 "Emergency_Contact_2": "9799492501/2"
+}
+];
+
+
+var defaultDataArray_2 = [
+{
+"Plaza_Name": "WELCOME TO MEGHAHIGHWAY RIDCOR AN INITIATIVE OF GOR AND IL&FS (Arjunsar to Pallu)",
+"Lane": "Lane1",
+"Shift": "Third Shift",
+"Vehicle_Class": "Multi Axle truck",
+"Journey": "Single",
+"Vehicle_Regn": "RJ09 RT0799",
+"Toll_Collector_Id": "5",
+"Amount_Paid": "Rs. 295.00",
+"Emergency_Contact": "1234567890"
 }
 ];
